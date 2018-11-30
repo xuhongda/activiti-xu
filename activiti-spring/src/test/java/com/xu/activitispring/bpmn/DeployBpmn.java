3d05b7355c0c3fbd2b920a123dfd34505f97601a
xuhongda <xuhongda7@outlook.com>
2018-11-30 14:42:42 +0800

    test

--- activiti-spring/src/test/java/com/xu/activitispring/bpmn/DeployBpmn.java
+++ activiti-spring/src/test/java/com/xu/activitispring/bpmn/DeployBpmn.java
@@ -3,6 +3,7 @@ package com.xu.activitispring.bpmn;
 import lombok.extern.slf4j.Slf4j;
 import org.activiti.engine.*;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,7 +24,8 @@ import java.util.Map;
 @RunWith(SpringRunner.class)
 public class DeployBpmn {
 
-
+    @Autowired
+    private ProcessEngine processEngine;
     @Autowired
     private RepositoryService repositoryService;
     @Autowired
@@ -40,7 +43,7 @@ public class DeployBpmn {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         RepositoryService repositoryService = processEngine.getRepositoryService();
         repositoryService.createDeployment()
-                .addClasspathResource("bpmn/xxx.bpmn20.xml")
+                .addClasspathResource("bpmn/myOne.bpmn20.xml")
                 .deploy();
 
         log.info("Number of process definitions: " + repositoryService.createProcessDefinitionQuery().count());
@@ -55,12 +58,21 @@ public class DeployBpmn {
         variables.put("employeeName", "Kermit");
         variables.put("numberOfDays", new Integer(4));
         variables.put("vacationMotivation", "I'm really tired!");
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
         RuntimeService runtimeService = processEngine.getRuntimeService();
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("vacationRequest", variables);
-        log.info("流程{}启动", processInstance.getName());
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my_activiti_one", variables);
+        log.info("流程{}启动", processInstance.getProcessDefinitionName());
         // Verify that we started a new process instance
         log.info("Number of process instances: " + runtimeService.createProcessInstanceQuery().count());
+    }
 
+    @Test
+    public void task() {
+        // Fetch all tasks for the management group
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("management").list();
+        for (Task task : tasks) {
+            log.info("Task available: " + task.getName());
+        }
     }
 }
