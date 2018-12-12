c0e80e0366dc92eb7f744f8243d69a1ab76dbab0
xuhongda <xuhongda7@outlook.com>
2018-12-12 11:29:05 +0800

    清楚非必需 spring 配置

--- activiti-spring/src/main/java/com/xu/activitispring/ActivitiSpringApplication.java
+++ activiti-spring/src/main/java/com/xu/activitispring/ActivitiSpringApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource(locations = {"classpath:config/activiti.context.xml"})
 public class ActivitiSpringApplication {
 
     public static void main(String[] args) {
